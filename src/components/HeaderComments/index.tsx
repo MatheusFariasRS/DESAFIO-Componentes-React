d69bd9409396a3b66fe64c9c5fff63ed5fff9bb9
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-25 14:27:04 -0300

    Header

---
+++ src/components/HeaderComments/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function HeaderComments(){
+    return(
+        <header>
+            <div className="ct-container">
+                <h1>DSCarrosTop</h1>
+            </div>
+        </header>
+    );
+}
---
+++ src/routes/CommentsPage/index.tsx
@@ -0,0 +1,10 @@
+import HeaderComments from "../../components/HeaderComments";
+import './styles.css'
+
+export default function CommentsPage(){
+    return(
+        <>
+        <HeaderComments/>
+        </>
+    );
+}
